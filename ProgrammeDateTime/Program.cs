6c7db6e1241ea530ae74330ce609663f54e73b07
AbdoulayePangnonKOUL <AbdoulayePangnonKOUL@TAL-7G6S793>
2023-11-30 23:06:37 +0100

    FirstCommit

---
+++ ProgrammeDateTime/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace ProgrammeDateTime
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+             DateTime date = DateTime.Now;
+            //   Console.WriteLine(date);
+            //    Console.WriteLine(date.Month);
+
+
+            CultureInfo cultureAnglais = CultureInfo.GetCultureInfo("en-US");
+            CultureInfo cultureFrancais = CultureInfo.GetCultureInfo("fr-FR");
+
+            //Console.WriteLine(date.ToString("dd/MM/yyyy"));
+            // Console.WriteLine(date.ToString("dd/MM/yyyy HH:mm:ss" ));
+            // Console.WriteLine(date.ToString("dddd MMMM yyyy HH:mm:ss"));
+            Console.WriteLine(date.ToString("dddd MMMM yyyy HH:mm:ss", cultureFrancais));
+
+            DateTime dateDemain = date.AddDays(1);
+
+            Console.WriteLine("Date de demain : " + dateDemain.ToString("dddd MMMM yyyyy"), cultureFrancais);
+
+            var diff = dateDemain - date;
+
+
+            Console.WriteLine("Différence de jour : "+ diff.TotalDays);
+            Console.WriteLine("Différence heure : " + diff.TotalHours);
+        }
+    }
+}
